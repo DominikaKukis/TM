5e0ef97c2cb098d60b5ea3da9caa222ef2f5aca9
Dominika K <kukisdominika@gmail.com>
2019-04-08 11:08:28 +0200

    CRUD

--- SalesCharts/Controllers/HomeController.cs
+++ SalesCharts/Controllers/HomeController.cs
@@ -26,5 +26,12 @@ namespace SalesCharts.Controllers
 
             return View();
         }
+
+        public ActionResult Product()
+        {
+            ViewBag.Message = "Your product page.";
+
+            return View();
+        }
     }
 }
---
+++ SalesCharts/db/Product.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] VARCHAR(50) NOT NULL, 
+    [Price] FLOAT NOT NULL
+)
