a1b969d31913831ed05d0c7fe66c13f2d84230f5
Dominika K <kukisdominika@gmail.com>
2019-05-19 20:09:31 +0200

    Added Inomce and demand controllers and views

--- SalesCharts/Controllers/ChartController.cs
+++ SalesCharts/Controllers/ChartController.cs
@@ -87,7 +87,7 @@ namespace SalesCharts.Controllers
             }
         }
 
-        public ActionResult Dashboard()
+        public ActionResult Dashboard() //Podaż -supply
         {
             DBModel dbModel = new DBModel();
 
@@ -98,14 +98,86 @@ namespace SalesCharts.Controllers
 
             var dates = listA.Select(x => x.Date).Distinct();
             
-
-
             ViewBag.AMOUNTA = amountA;
             ViewBag.AMOUNTB = amountB;
             ViewBag.DATES = dates;
-            ViewBag.REP = amountA;
 
+            return View();
+        }
 
+        public ActionResult Demand() //Popyt
+        {
+            DBModel dbModel = new DBModel();
+            Random rand = new Random();
+            
+            var listA = dbModel.Product.Where(x => x.Name == "ProductA").ToList();
+            var amountA = listA.Select(x => x.Amount);
+
+            var listAA = new List<int>();
+
+            for(int i=0; i < amountA.Count(); i++)
+            {
+                var a = rand.Next(1000) + 100;
+                listAA.Add(a);
+            }
+
+            var listB = dbModel.Product.Where(x => x.Name == "ProductB").ToList();
+            var amountB = listB.Select(x => x.Amount);
+
+            var listBB = new List<int>();
+
+            for (int i = 0; i < amountB.Count(); i++)
+            {
+                var a = rand.Next(1000) + 100;
+                listBB.Add(a);
+            }
+
+            var dates = listA.Select(x => x.Date).Distinct();
+
+            ViewBag.LISTAA = listAA;
+            ViewBag.LISTBB = listBB;
+            ViewBag.DATES = dates;
+
+            return View();
+        }
+
+        public ActionResult Income() //Przychód
+        {
+            DBModel dbModel = new DBModel();
+
+            var listA = dbModel.Product.Where(x => x.Name == "ProductA").ToList();
+            var incomeA = new List<double>();
+
+            for (int i = 0; i < listA.Count(); i++)
+            {
+                var a = listA.Where(x => x.Id == (i + 1)).First();
+                double price = a.Price;
+                double provision = a.Provision;
+                double sum = price + provision;
+                incomeA.Add(sum);
+
+            }
+
+            var listB = dbModel.Product.Where(x => x.Name == "ProductB").ToList();
+            var incomeB = new List<double>();
+
+            for (int i = 0; i < listB.Count(); i++)
+            {
+                var b = listB[i];
+                if (b != null)
+                {
+                    double price = b.Price;
+                    double provision = b.Provision;
+                    double sum = price + provision;
+                    incomeB.Add(sum);
+                }
+            }
+
+            var dates = listA.Select(x => x.Date).Distinct();
+
+            ViewBag.DATES = dates;
+            ViewBag.incomeA = incomeA;
+            ViewBag.incomeB = incomeB;
             return View();
         }
     }
